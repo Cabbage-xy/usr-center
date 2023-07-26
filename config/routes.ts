b83de2e5fca3971a731c8770f34a501c4d30fdc3
Cabbage-xy <cxy4915@gmail.com>
2023-07-26 20:59:05 +0800

    refactor: installed umi-ui, removed i18n

--- config/routes.ts
+++ config/routes.ts
@@ -1,52 +1,20 @@
-﻿export default [
+export default [
   {
     path: '/user',
     layout: false,
-    routes: [
-      {
-        name: 'login',
-        path: '/user/login',
-        component: './user/Login',
-      },
-      {
-        component: './404',
-      },
-    ],
+    routes: [{ path: '/user/login', component: './user/Login' }, { component: './404' }],
   },
-  {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
+  { path: '/welcome', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
-    name: 'admin',
     icon: 'crown',
     access: 'canAdmin',
     routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-      {
-        component: './404',
-      },
+      { path: '/admin/sub-page', icon: 'smile', component: './Welcome' },
+      { component: './404' },
     ],
   },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-  },
-  {
-    path: '/',
-    redirect: '/welcome',
-  },
-  {
-    component: './404',
-  },
+  { icon: 'table', path: '/list', component: './TableList' },
+  { path: '/', redirect: '/welcome' },
+  { component: './404' },
 ];
